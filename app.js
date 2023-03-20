c52feabdec6348c34c47460ae53be5f8951167fc
EXECUTIVEGS-6 <EXECUTIVEGS-6@fivesdigital.com>
2023-03-20 09:38:33 +0530

    added

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-// app.js
 Quagga.init({
-    inputStream: {
-      name: "Live",
-      type: "LiveStream",
-      target: document.querySelector("#video")
+    inputStream : {
+      name : "Live",
+      type : "LiveStream",
+      target: document.querySelector('#yourElement')    // Or '#yourElement' (optional)
     },
-    decoder: {
-      readers: ["code_128_reader"]
+    decoder : {
+      readers : ["code_128_reader"]
     }
   }, function(err) {
-    if (err) {
-      console.error(err);
-      alert(err)
-      return;
-    }
-    console.log("QuaggaJS ready to start.");
-    alert("QuaggaJS ready to start.");
-    Quagga.start();
+      if (err) {
+          alert(err);
+          return
+      }
+      alert("Initialization finished. Ready to start");
+      Quagga.start();
   });
-  
-  Quagga.onDetected(function(result) {
-    alert("Barcode detected:", result.codeResult.code);
-  });
-  
