67e8247d0b9c909a9b286d8f783ab5e615edb629
Mohd Shahid <72098918+shahid1223@users.noreply.github.com>
2023-03-24 14:49:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
-Quagga.init({
-    inputStream : {
-      name : "Live",
-      type : "LiveStream",
-      target: document.querySelector('#video')    // Or '#yourElement' (optional)
-    },
-    decoder : {
-      readers : ["code_128_reader"]
-    }
-  }, function(err) {
-      if (err) {
-          alert(err);
-          return
+window.addEventListener('load', function () {
+    let selectedDeviceId;
+    var hints = new Map();
+    hints.set(ZXing.DecodeHintType.ASSUME_GS1, true)
+    hints.set(ZXing.DecodeHintType.TRY_HARDER, true)
+    const codeReader = new ZXing.BrowserMultiFormatReader(hints)
+    alert('ZXing code reader initialized')
+    codeReader.getVideoInputDevices()
+      .then((videoInputDevices) => {
+      const sourceSelect = document.getElementById('sourceSelect')
+      selectedDeviceId = videoInputDevices[0].deviceId
+      if (videoInputDevices.length >= 1) {
+        videoInputDevices.forEach((element) => {
+          const sourceOption = document.createElement('option')
+          sourceOption.text = element.label
+          sourceOption.value = element.deviceId
+          sourceSelect.appendChild(sourceOption)
+        })
+
+        sourceSelect.onchange = () => {
+          selectedDeviceId = sourceSelect.value;
+        };
+
+        const sourceSelectPanel = document.getElementById('sourceSelectPanel')
+        sourceSelectPanel.style.display = 'block'
       }
-      alert("Initialization finished. Ready to start");
-      Quagga.start();
-  });
+
+      document.getElementById('startButton').addEventListener('click', () => {
+        codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
+          if (result) {
+            alert(result.getText())
+            document.getElementById('result').textContent = result.text
+          }
+          if (err && !(err instanceof ZXing.NotFoundException)) {
+            console.error(err)
+            document.getElementById('result').textContent = err
+          }
+        })
+        alert(`Started continous decode from camera with id ${selectedDeviceId}`)
+      })
+
+      document.getElementById('resetButton').addEventListener('click', () => {
+        codeReader.reset()
+        document.getElementById('result').textContent = '';
+        alert('Reset.')
+      })
+
+    })
+      .catch((err) => {
+      console.error(err)
+    })
+  })
